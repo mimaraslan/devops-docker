05f77e20fad3e79edda8c2f1d51fef0fd6b2048e
mimaraslan <mimaraslan@gmail.com>
2022-04-12 23:38:51 +0300

    DevOps Docker
    DevOps Docker

--- part4/example03/py/index.py
+++ part4/example03/py/index.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Merhaba Arkadaslar, A'dan Z'ye Docker Egitimine hosgeldiniz"
+    return "Merhaba hosgeldiniz"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
